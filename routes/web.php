60fa8f1171a18751c9549f1823fba90829a94798
Habib Hossain <habibfahim71@gmail.com>
2026-04-11 18:05:12 +0600

    -

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\ReportController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/one-to-one', [TestController::class, 'oneToOne']);
 Route::get('/one-to-many', [TestController::class, 'oneToMany']);
