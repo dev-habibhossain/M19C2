64ed56974a0921b348e2b725bbad588102aa3e19
Habib Hossain <habibfahim71@gmail.com>
2026-04-11 18:04:33 +0600

    reportcontroller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use App\Models\Order;
+use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class ReportController
@@ -21,7 +23,22 @@ class ReportController
       $ordersQuery = Order::query()->betweenDates($from, $to);
 
 
-      //Total sales
+      //Total customer
+      $totalCustomers = Customer::count();
 
+      //Total orders
+      $totalOrders = (clone $ordersQuery)->count();
+      $totalRevenue = (clone $ordersQuery)->sum('grand_total');
+
+      $totalPaid = Payment::whereHas('order', function($query) use ($from, $to){
+        $query->betweenDates($from, $to);
+      })->sum('amount');
+
+      return response()->json([
+        "total_customers" => $totalCustomers,
+        "total_orders" => $totalOrders,
+        "total_revenue" => $totalRevenue,
+        "total_paid" => $totalPaid
+      ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\ReportController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/one-to-one', [TestController::class, 'oneToOne']);
 Route::get('/one-to-many', [TestController::class, 'oneToMany']);
