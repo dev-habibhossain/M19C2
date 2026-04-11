ebec97b2304d9a7d5544b18fad75051013a0443e
Habib Hossain <habibfahim71@gmail.com>
2026-04-10 21:15:32 -0400

    1:34:00

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class ReportController
 {
-    
+    public function summary(Request $request)
+    {
+      $from = $request->query("from");
+      $to = $request->query("to");
+
+      if(!$from || !$to){
+        $to = now()->toDateString();
+        $from = now()->subDays(30)->toDateString();
+      }
+
+      //Base query
+      $ordersQuery = Order::query()->betweenDates($from, $to);
+
+
+      //Total sales
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +13,5 @@ Route::get('/one-to-many', [TestController::class, 'oneToMany']);
 Route::get('/many-to-many', [TestController::class, 'manyToMany']);
 Route::get('/product-with-cat', [TestController::class, 'productWithCat']);
 Route::get('/selfRef', [TestController::class,'selfRef']);
+
+Route::get('/summary', [ReportController::class, 'summary']);
